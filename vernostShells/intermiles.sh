0d54be017db2df5ff93bf15cd24f4ce3ab5dec50
rohansolse <40458275+rohansolse@users.noreply.github.com>
2020-02-19 09:55:03 +0530

    new changes in intermiles shell

--- vernostShells/intermiles.sh
+++ vernostShells/intermiles.sh
@@ -3,11 +3,11 @@
 # Tab for workspace
 gnome-terminal --tab --active
 # open new tab and run UBBusiness.js with nodemon
-gnome-terminal --tab -- bash -c "sudo nodemon ubweb/Connector/Airlines-buisness/UBBusiness.js; exec bash"
+gnome-terminal --tab -- bash -c "cd ubweb/Connector/Airlines-buisness; sudo nodemon UBBusiness.js; exec bash"
 # open new tab and run UBConnector.js with nodemon
-gnome-terminal --tab -- bash -c "sudo nodemon ubweb/Connector/Airlines-Connector/UBConnector.js; exec bash"
+gnome-terminal --tab -- bash -c "scd ubweb/Connector/Airlines-Connector; sudo nodemon UBConnector.js; exec bash"
 # open new tab and run daoapp.js with nodemon
-gnome-terminal --tab -- bash -c "sudo nodemon ubweb/Connector/Airlines-DAO/daoapp.js; exec bash"
+gnome-terminal --tab -- bash -c "cd ubweb/Connector/Airlines-DAO; sudo nodemon daoapp.js; exec bash"
 # open vscode with our project then frontend on browser and run redis
 gnome-terminal --tab -- bash -c "code ubweb; xdg-open http://localhost:8080/; redis-server; exec bash"
 # this will start frontend
