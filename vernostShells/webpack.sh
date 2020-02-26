36fa73e6d45bf102b8d2d9e36d429b69b9deed21
rohansolse <rohansolse@gmail.com>
2020-02-26 10:21:40 +0530

    add comments

--- vernostShells/webpack.sh
+++ vernostShells/webpack.sh
@@ -1,3 +1,4 @@
+#Asking for builds
 echo "Please let me know which build you want [business | dao | connector | frontend | all]: "
 read toBuild
 echo "You Selected: $toBuild"
@@ -6,33 +7,33 @@ if [ ! -d builds ]
 then
     mkdir builds
 fi
+#Create Business build
 if [ $toBuild = "business" ] || [ $toBuild = "all" ]
 then
     echo "Start creating Business Build"
     cd ubweb/Connector/Airlines-buisness
-    rm -rf build/
     webpack
     cd build
     cp businessapp.js ../../../../builds
     cd ../../../../
     echo "Build for business is created"
 fi
+#Create Connector build
 if [ $toBuild = "connector" ] || [ $toBuild = "all" ]
 then
     echo "Start creating Connector Build"
     cd ubweb/Connector/Airlines-Connector
-    rm -rf build/
     webpack
     cd build
     cp connectorapp.js ../../../../builds
     cd ../../../../
     echo "Build for connector is created"
 fi
+#Create Dao build
 if [ $toBuild = "dao" ] || [ $toBuild = "all" ]
 then
     echo "Start creating Dao Build"
     cd ubweb/Connector/Airlines-DAO
-    rm -rf build/
     webpack
     cd build
     cp daoapp.js ../../../../builds
