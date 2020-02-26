ce7df5dab060692cd97147c854fdc11fea4d64bf
rohansolse <rohansolse@gmail.com>
2020-02-26 13:05:21 +0530

    add frontend script

--- vernostShells/webpack.sh
+++ vernostShells/webpack.sh
@@ -1,5 +1,4 @@
-#Asking for builds
-echo "Please let me know which build you want [business | dao | connector | frontend | all]: "
+echo "Please let me know which build you want [ business | dao | connector | frontend | all ]: \c"
 read toBuild
 echo "You Selected: $toBuild"
 #To create directory
@@ -29,7 +28,7 @@ then
     cd ../../../../
     echo "Build for connector is created"
 fi
-#Create Dao build
+#Create Dao Build
 if [ $toBuild = "dao" ] || [ $toBuild = "all" ]
 then
     echo "Start creating Dao Build"
@@ -39,4 +38,15 @@ then
     cp daoapp.js ../../../../builds
     cd ../../../../
     echo "Build for dao is created"
+fi
+#Create Frontend Build
+if [ $toBuild = "frontend" ] || [ $toBuild = "all" ]
+then
+    echo "Start creating Frontend Build"
+    cd ubweb
+    npm run build:ssr
+    zip -r ubdist.zip dist node_modules
+    cp ubdist.zip ../builds
+    cd ..
+    echo "Build for Frontend is created"
 fi
