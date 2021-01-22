ac0d9b5f17ec9662835c8256a73e28fba613c58e
Rohan Solse <rohan.solse@vernost.in>
2021-01-22 14:23:34 +0530

    add clean.sh

---
+++ clean.sh
@@ -0,0 +1,19 @@
+cd Pictures
+ls
+# if test -f *.png;
+# then
+    rm *.png
+    echo "All .png Deleted"
+# fi
+
+# if test -f *.jpeg
+# then
+    rm *.jpeg
+    echo "All .jpeg Deleted"
+# fi
+
+# if test -f *.jpg
+# then
+    rm *.jpg
+    echo "All .jpg Deleted"
+# fi
