e86a6558fd38f52d5df0433b8a79b64d04aee50d
Rohan Solse <rohan.solse@vernost.in>
2021-01-22 11:13:23 +0530

    add new shell

---
+++ renameFiles.sh
@@ -0,0 +1,10 @@
+# renaming file from specific no
+
+cd Downloads/Telegram\ Desktop/
+ls
+# a=300
+# for i in *.jpg; do
+#   new=$(printf "%01d.jpg" "$a") #04 pad to length of 4
+#   mv -i -- "$i" "$new"
+#   a=$(( $a + 1 ))
+# done
